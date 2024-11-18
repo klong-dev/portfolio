5050db53dfd5f3b128b7d5cf47e40a8a36c29718
Hoàng Kim Long <longhoang8204@gmail.com>
2024-11-18 09:18:26 +0700

    feat: save theme

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -13,13 +13,20 @@ export const ThemeProvider: React.FC<ThemeProviderType> = ({ children }) => {
   const [isDark, setIsDark] = React.useState(true);
 
   React.useEffect(() => {
-    const isDarkMode = document.documentElement.classList.contains("dark");
-    setIsDark(isDarkMode);
+    const localTheme = localStorage.getItem("theme");
+    if (localTheme) {
+      document.documentElement.classList.add(localTheme);
+      setIsDark(localTheme === "dark");
+    } else {
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+    }
   }, []);
 
   const toggle = () => {
     document.documentElement.classList.toggle("dark");
     setIsDark(!isDark);
+    localStorage.setItem("theme", isDark ? "light" : "dark");
   };
 
   return <ThemeContext.Provider value={{ isDark, toggle }}>{children}</ThemeContext.Provider>;
